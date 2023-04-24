ea7b4590040bba02c7b38148dbc1620dfc7b3cc2
SaiRameshkumar14 <saiddiwaker141102@gmail.com>
2023-04-24 14:55:54 +0530

    updates

--- app.py
+++ app.py
@@ -8,21 +8,26 @@ app = Flask(__name__, template_folder="template")
 def index():
     return render_template("home.html")
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+
 @app.route('/skills')
-def about():
+def skills():
     return render_template("skills.html")
 
+
 @app.route('/profile')
-def about():
+def profile():
     return render_template("profile.html")
 
+
 @app.route('/experience')
-def about():
+def experience():
     return render_template("Experience.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
