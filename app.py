d0327e8346e2b481f7764e468b78f484811c610e
SaiRameshkumar14 <saiddiwaker141102@gmail.com>
2023-04-24 14:41:23 +0530

    updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="template")
 
@@ -8,6 +8,21 @@ app = Flask(__name__, template_folder="template")
 def index():
     return render_template("home.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/skills')
+def about():
+    return render_template("skills.html")
+
+@app.route('/profile')
+def about():
+    return render_template("profile.html")
+
+@app.route('/experience')
+def about():
+    return render_template("Experience.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
